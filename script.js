e75b7e59b65b6926759654e61248803abe2179c2
DEV VIKNESH A D <164396475+Devviknesh@users.noreply.github.com>
2025-01-31 22:06:43 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+async function getQuote() {
+    let response = await fetch("https://api.quotable.io/random");
+    let data = await response.json();
+    document.getElementById("quote").innerText = data.content;
+}
